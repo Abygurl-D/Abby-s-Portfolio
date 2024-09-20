5b41d195626651c43f74229797577f3244fcb0f1
Abygurl <thehairapist123@gmail.com>
2024-09-20 09:15:44 +0000

    footer created

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-jQuery('.navbar').fadeTo( "slow" , navOpacity);
+// jQuery('.navbar').fadeTo( "slow" , navOpacity);
 
-if (jQuery('nav').css('opacity') < 1) {
-  jQuery('.navigation').fadeTo( "slow" , 1);
-};
+// if (jQuery('nav').css('opacity') < 1) {
+//   jQuery('.navigation').fadeTo( "slow" , 1);
+// };
+
+// const aboutCard = document.querySelector(".about-card3");
+
+// aboutCard.onmouseenter = function () {
+//   aboutCard.classList.add("scale-in-hor-right");
+// };
+
+// aboutCard.addEventListener("mouseenter", function () {
+//   aboutCard.classList.add("scale-in-hor-right");
+// });
